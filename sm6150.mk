419fb60bace1dd4be2c530f3e1f5098016826e56
CryllicBuster273 <cryllicbuster273@pixelexperience.org>
2020-07-24 23:44:33 +0530

    Revert "sm6150-common: Move libperfmgr to common"
    This reverts commit 4ca969ebb1fad79f0bfbf6974e9340109f582e2e.
    
    Signed-off-by: CryllicBuster273 <cryllicbuster273@pixelexperience.org>

--- sm6150.mk
+++ sm6150.mk
@@ -300,7 +300,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.3-service.xiaomi-libperfmgr
+    android.hardware.power@1.3-service.sm6150-libperfmgr \
+    powerhint.json
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf
