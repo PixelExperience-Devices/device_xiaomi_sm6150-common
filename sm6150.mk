58451e57a4e64c3aa037089f0a8187312662e19f
CryllicBuster273 <cryllicbuster273@pixelexperience.org>
2020-07-27 20:48:29 +0530

    Revert "sm6150-common: Build common thermal HAL"
    This reverts commit 31ac58b3bca910b5e722e32a4cf55f2e40b9e7c2.

--- sm6150.mk
+++ sm6150.mk
@@ -398,7 +398,9 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0-service.pixel
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service \
+    thermal.sm6150
 
 # Touchscreen
 PRODUCT_PACKAGES += \
