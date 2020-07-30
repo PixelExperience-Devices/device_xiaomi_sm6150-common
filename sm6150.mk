sm6150-dev/android_device_xiaomi_sm6150-common

sm6150-common: Build common thermal HAL
Change-Id: I140be2a9307e8cbf475c35fb561dd675f6458c4d

sm6150.mk
@@ -381,9 +381,7 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service \
-    thermal.sm6150
+    android.hardware.thermal@2.0-service.xiaomi
 
 # Touchscreen
 PRODUCT_PACKAGES += \

PixelExperience-Devices/device_xiaomi_sm6150-common

Signed-off-by: CryllicBuster273 <cryllicbuster273@pixelexperience.org>

sm6150.mk
@@ -382,9 +382,7 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service \
-    thermal.sm6150
+    android.hardware.thermal@2.0-service.pixel
 
 # Touchscreen
 PRODUCT_PACKAGES += \
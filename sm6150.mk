6d8bfe06953480ec4f5812549627d68b7a72b13a
Pig <pig.priv@gmail.com>
2020-07-20 22:56:37 +0200

    sm6150-common: Build common thermal HAL
    Change-Id: I140be2a9307e8cbf475c35fb561dd675f6458c4d
    Signed-off-by: CryllicBuster273 <cryllicbuster273@pixelexperience.org>

--- sm6150.mk
+++ sm6150.mk
@@ -382,9 +382,7 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service \
-    thermal.sm6150
+    android.hardware.thermal@2.0-service.pixel
 
 # Touchscreen
 PRODUCT_PACKAGES += \
