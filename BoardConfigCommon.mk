d2165b49ae3661e892c9aa07731d8b9c3271718c
LuK1337 <priv.luk@gmail.com>
2020-08-01 01:07:57 +0200

    sm6150-common: Pass proper boot image header version
    Change-Id: I0f8b702c4892addb1896b6be5b7be98c8b5b3097
    Signed-off-by: CryllicBuster273 <cryllicbuster273@pixelexperience.org>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -35,6 +35,11 @@ BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2
 
 # Kernel
+ifneq ($(PRODUCT_USE_DYNAMIC_PARTITIONS), true)
+BOARD_BOOT_HEADER_VERSION := 2 
+else
+BOARD_BOOT_HEADER_VERSION := 1
+endif
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.console=ttyMSM0 service_locator.enable=1 swiotlb=1 earlycon=msm_geni_serial,0x880000 loop.max_part=7 cgroup.memory=nokmem,nosocket
 BOARD_KERNEL_CMDLINE +=  androidboot.vbmeta.avb_version=1.0
 BOARD_KERNEL_OFFSET := 0x00008000
@@ -42,6 +47,7 @@ BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_SEPARATED_DTBO := true
+BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sm6150
 TARGET_KERNEL_CLANG_COMPILE := true
