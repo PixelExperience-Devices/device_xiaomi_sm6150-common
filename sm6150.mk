5c83bea61bbd2784e2509552a9b2468181430f65
Edwin Wong <edwinwong@google.com>
2021-03-25 14:26:21 -0700

    sm6150-common: Update clearkey plugin to drm HAL v1.4
    Test: atest
      atest CtsMediaTestCases:android.media.cts.MediaDrmClearkeyTest#testGetPlaybackComponent
    
    Bug: 182525516
    Change-Id: I7f56ca5a4038b85fcba131edd9ba3038a2b778fc

--- sm6150.mk
+++ sm6150.mk
@@ -168,7 +168,7 @@ PRODUCT_COPY_FILES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.3-service.clearkey \
+    android.hardware.drm@1.4-service.clearkey \
     android.hardware.drm@1.3.vendor
 
 # fastbootd
