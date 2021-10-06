06990fc28b55c958255f7bb88f4f3fbbd5bf5e75
Marijn Suijten <marijn.suijten@somainline.org>
2021-10-06 22:44:02 +0200

    sm6150-common: Provide android.system.net.netd@1.1 on vendor for netmgrd
    Following the previous commit, starting with Android S most HAL
    libraries are not included in VNDK anymore - they need to be explicitly
    pulled in to `/vendor` for odm blobs to be allowed to access them:
    
        linker  : CANNOT LINK EXECUTABLE "/odm/bin/netmgrd": library "android.system.net.netd@1.1.so" not found: needed by main executable
    
    Signed-off-by: Marijn Suijten <marijn.suijten@somainline.org>
    Change-Id: Ide8adad466a61412a984a46d1562f8f2f6a62be3

--- sm6150.mk
+++ sm6150.mk
@@ -279,6 +279,9 @@ PRODUCT_COPY_FILES += \
     hardware/qcom-caf/sm8150/media/conf_files/sm6150/system_properties.xml:$(TARGET_COPY_OUT_VENDOR)/etc/system_properties.xml
 
 # Network
+PRODUCT_PACKAGES += \
+    android.system.net.netd@1.1.vendor
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
 
